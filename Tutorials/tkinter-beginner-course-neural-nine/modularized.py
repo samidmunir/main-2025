30df33e69e10fc64103c36820060a2f241583a6a
Sami Munir <sami.munir2001@gmail.com>
2024-10-07 01:21:13 -0400

    learning & implementing event handlers on elements + gracefully closing tkinter app with confirmation --> fully functional GUI complete

--- Tutorials/tkinter-beginner-course-neural-nine/modularized.py
+++ Tutorials/tkinter-beginner-course-neural-nine/modularized.py
@@ -9,6 +9,7 @@ class MyGUI:
         self.label.pack(padx = 10, pady = 10)
         
         self.textbox = tk.Text(self.root, height = 5, font = ('Helvetica', 18))
+        self.textbox.bind('<KeyPress>', self.shortcut)
         self.textbox.pack(padx = 10, pady = 10)
         
         self.check_state = tk.IntVar()
@@ -19,6 +20,8 @@ class MyGUI:
         self.button = tk.Button(self.root, text = 'Show Message', font = ('Helvetica', 18), command = self.show_message)
         self.button.pack(padx = 10, pady = 10)
         
+        self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
+        
         self.root.mainloop()
     
     def show_message(self):
@@ -26,5 +29,15 @@ class MyGUI:
             print(self.textbox.get('1.0', tk.END))
         else:
             messagebox.showinfo('Message', self.textbox.get('1.0', tk.END))
+
+    def shortcut(self, event):
+        # print('\nevent.state:', event.state)
+        # print('event.keysym:', event.keysym)
+        if event.state == 4 and event.keysym == 'Return':
+            self.show_message()
+    
+    def on_closing(self):
+        if messagebox.askyesno(title = 'Quit?', message = 'Do you really want to quit?'):
+            self.root.destroy()
         
 MyGUI()
