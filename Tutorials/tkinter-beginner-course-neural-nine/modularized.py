53a044f9871dcf014543fc094d3914adaecc00df
Sami Munir <sami.munir2001@gmail.com>
2024-10-07 01:33:05 -0400

    GUI app fully complete using tkinter: adding functionality to clear the textbox

--- Tutorials/tkinter-beginner-course-neural-nine/modularized.py
+++ Tutorials/tkinter-beginner-course-neural-nine/modularized.py
@@ -5,6 +5,20 @@ class MyGUI:
     def __init__(self):
         self.root = tk.Tk()
         
+        self.menubar = tk.Menu(self.root)
+        
+        self.filemenu = tk.Menu(self.menubar, tearoff = 0)
+        self.filemenu.add_command(label = 'Close', command = self.on_closing)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label = 'Close without confirmation', command = exit)
+        
+        self.actionmenu = tk.Menu(self.menubar, tearoff = 0)
+        self.actionmenu.add_command(label = 'Show Message', command = self.show_message)
+        
+        self.menubar.add_cascade(menu = self.filemenu, label = 'File')
+        self.menubar.add_cascade(menu = self.actionmenu, label = 'Action')
+        self.root.config(menu = self.menubar)
+        
         self.label = tk.Label(self.root, text = 'Your Message', font = ('Helvetica', 18))
         self.label.pack(padx = 10, pady = 10)
         
@@ -19,6 +33,9 @@ class MyGUI:
         
         self.button = tk.Button(self.root, text = 'Show Message', font = ('Helvetica', 18), command = self.show_message)
         self.button.pack(padx = 10, pady = 10)
+        
+        self.clearbutton = tk.Button(self.root, text = 'Clear', font = ('Helvetica', 18), command = self.clear)
+        self.clearbutton.pack(padx = 10, pady = 10)
         
         self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
         
@@ -40,4 +57,8 @@ class MyGUI:
         if messagebox.askyesno(title = 'Quit?', message = 'Do you really want to quit?'):
             self.root.destroy()
         
+        
+    def clear(self):
+        self.textbox.delete('1.0', tk.END)
+
 MyGUI()
