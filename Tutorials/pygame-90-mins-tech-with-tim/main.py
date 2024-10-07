2bc22097fea8a5ca3439aadab766f3f41699a7e6
Sami Munir <sami.munir2001@gmail.com>
2024-10-07 19:11:17 -0400

    implementing 60fps pygame loop using pygame.time.Clock() & clock.tick(FPS) + adding spaceship images (scaling + rotating) --> onto WINDOW

--- Tutorials/pygame-90-mins-tech-with-tim/main.py
+++ Tutorials/pygame-90-mins-tech-with-tim/main.py
@@ -1,12 +1,40 @@
 import pygame
+import os
 
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 900, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")
+FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+def draw_window():
+    WINDOW.fill('#262626')
+    
+    # Use .blit() when to draw a surface onto the WINDOW.
+    WINDOW.blit(YELLOW_SPACESHIP, (300, 100))
+    WINDOW.blit(RED_SPACESHIP, (700, 100))
+    
+    pygame.display.update()
+
+
 
 def main():
+    clock = pygame.time.Clock()
     running = True
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        draw_window()
     pygame.quit()
+    
+if __name__ == '__main__':
+    main()
