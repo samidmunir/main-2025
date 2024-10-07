09e7f5d3870db6f77af35655f383ac022c8122d9
Sami Munir <sami.munir2001@gmail.com>
2024-10-06 22:51:38 -0400

    implementing grid layout with tkinter

--- Tutorials/tkinter-beginner-course-neural-nine/main.py
+++ Tutorials/tkinter-beginner-course-neural-nine/main.py
@@ -16,7 +16,32 @@ textbox.pack(padx = 20, pady = 20)
 # entry = tk.Entry(root)
 # entry.pack()
 
-button = tk.Button(root, text = 'Click Me!', font = ('Helvetica', 18))
-button.pack(padx = 10, pady = 10)
+# button = tk.Button(root, text = 'Click Me!', font = ('Helvetica', 18))
+# button.pack(padx = 10, pady = 10)
+
+buttonFrame = tk.Frame(root)
+buttonFrame.columnconfigure(0, weight = 1)
+buttonFrame.columnconfigure(1, weight = 1)
+buttonFrame.columnconfigure(2, weight = 1)
+
+button1 = tk.Button(buttonFrame, text = '1', font = ('Helvetica', 18))
+button1.grid(row = 0, column = 0, sticky = tk.W + tk.E)
+
+button2 = tk.Button(buttonFrame, text = '2', font = ('Helvetica', 18))
+button2.grid(row = 0, column = 1, sticky = tk.W + tk.E)
+
+button3 = tk.Button(buttonFrame, text = '3', font = ('Helvetica', 18))
+button3.grid(row = 0, column = 2, sticky = tk.W + tk.E)
+
+button4 = tk.Button(buttonFrame, text = '4', font = ('Helvetica', 18))
+button4.grid(row = 1, column = 0, sticky = tk.W + tk.E)
+
+button5 = tk.Button(buttonFrame, text = '5', font = ('Helvetica', 18))
+button5.grid(row = 1, column = 1, sticky = tk.W + tk.E)
+
+button6 = tk.Button(buttonFrame, text = '6', font = ('Helvetica', 18))
+button6.grid(row = 1, column = 2, sticky = tk.W + tk.E)
+
+buttonFrame.pack(fill = 'x')
 
 root.mainloop()
