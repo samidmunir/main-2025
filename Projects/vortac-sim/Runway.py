77292e375beef15aa3fadabe93abcef4156baa98
Sami Munir <sami.munir2001@gmail.com>
2024-10-08 00:12:00 -0400

    refactoring and updating code for projec vortac-sim --> will be utilizing pygame

--- Projects/vortac-sim/Runway.py
+++ Projects/vortac-sim/Runway.py
@@ -1,5 +1,5 @@
 class Runway:
-    def __init__(self, name: str, lenght: float, heading: float, start_x_loc: float, start_y_loc: float,        end_x_loc: float, end_y_loc: float, active: bool = True):
+    def __init__(self, name: str, lenght: float, heading: float, start_x_loc: float, start_y_loc: float, end_x_loc: float, end_y_loc: float, active: bool = True):
         print('\nRunway.__init__() called -->')
         if name is None or len(name) < 2:
             print('***ERROR***: Runway initialization failed')
@@ -14,3 +14,30 @@ class Runway:
         self.end_y_loc = end_y_loc
         self.active = active
         print('\tRunway initialized successfully.')
+    
+    def update_name(self, new_name: str):
+        print('\nRunway.update_name() called -->')
+        if new_name is None or len(new_name) < 2:
+            print('***ERROR***: Failed to update runway name')
+            print('\tInvalid name. Name must be at least 2 characters long.')
+            return
+        self.name = new_name
+        print('\tRunway name updated successfully.')
+    
+    def update_lenght(self, new_lenght: float):
+        print('\nRunway.update_lenght() called -->')
+        if new_lenght is None or new_lenght <= 0:
+            print('***ERROR***: Failed to update runway length')
+            print('\tInvalid length. Length must be a positive numeric value.')
+            return
+        self.lenght = new_lenght
+        print('\tRunway length updated successfully.')
+    
+    def update_heading(self, new_heading: float):
+        print('\nRunway.update_heading() called -->')
+        if new_heading is None or (new_heading < 0 or new_heading > 360):
+            print('***ERROR***: Failed to update runway heading')
+            print('\tInvalid heading. Heading must be between 0 and 360 degrees.')
+            return
+        self.heading = new_heading
+        print('\tRunway heading updated successfully.')
