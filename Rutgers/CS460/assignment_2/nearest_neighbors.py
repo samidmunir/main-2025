d3bc8a7a8f7c508b1f03d502dce7178871c2a78a
Sami Munir <sami.munir2001@gmail.com>
2024-10-10 01:20:44 -0400

    implementing function find_nearest_neighbors for target configuration given a value k for # of nearest neighbors and a list of configurations

--- Rutgers/CS460/assignment_2/nearest_neighbors.py
+++ Rutgers/CS460/assignment_2/nearest_neighbors.py
@@ -11,3 +11,17 @@ def load_configurations(filename):
         configurations = [list(map(float, line.strip().split())) for line in file]
     
     return configurations
+
+# Nearest neighbor search using linear search.
+def find_nearest_neighbors(target, configurations, k):
+    distances = []
+    
+    # Calculate distance from target to each configuration.
+    for config in configurations:
+        dist  = euclidean_distance(target, config)
+        distances.append((config, dist))
+        
+    # Sort by distance and return the k-nearest configurations.
+    distances.sort(key=lambda x: x[1])
+    
+    return [config for config, dist in distances[:k]]
