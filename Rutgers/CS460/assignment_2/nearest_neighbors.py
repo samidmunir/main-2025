a64b94e0c2f44fb8b9d0101285336227d702dd2c
Sami Munir <sami.munir2001@gmail.com>
2024-10-10 01:16:52 -0400

    implementing component_2.load_configurations() to load and parse configurations from config file

--- Rutgers/CS460/assignment_2/nearest_neighbors.py
+++ Rutgers/CS460/assignment_2/nearest_neighbors.py
@@ -1,5 +1,13 @@
 import argparse
 import numpy as NP
 
+# Function to compute the Euclidean distance between two configurations.
 def euclidean_distance(config1, config2):
     return NP.linalg.norm(NP.array(config1) - NP.array(config2))
+
+# Parse the configurations from the file.
+def load_configurations(filename):
+    with open(filename, 'r') as file:
+        configurations = [list(map(float, line.strip().split())) for line in file]
+    
+    return configurations
