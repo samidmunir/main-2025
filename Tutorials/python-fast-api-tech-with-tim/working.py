7104325b4b043bfaa454013383941c6e9e7d6145
Sami Munir <sami.munir2001@gmail.com>
2024-10-10 23:54:59 -0400

    adding GET endpoint to return entire inventory

--- Tutorials/python-fast-api-tech-with-tim/working.py
+++ Tutorials/python-fast-api-tech-with-tim/working.py
@@ -1,8 +1,35 @@
+"""
+    Data exchange over HTTP protocol gets converted to JSON.
+    - FastAPI automatically parses JSON data to internal Python types.
+"""
+
 from fastapi import FastAPI
 
 # API initialization using FastAPI.
 app = FastAPI()
 
+# Using a grocery store's inventory as an example.
+inventory = {
+    1: {
+        'name': 'Milk',
+        'price': 3.99,
+        'brand': 'Kirkland'
+    },
+    2: {
+        'name': 'Bread',
+        'price': 2.49,
+        'brand': 'Hearty Buns'
+    }
+}
+
+# Default endpoint for the API.
 @app.get('/')
 def home():
     return {'Data': 'Test'}
+
+# Endpoint for getting the inventory.
+@app.get('/get-items')
+def get_items():
+    return inventory
+
+# Endpoint for getting a specific item from the inventory using its ID.
