9716cad6902b807b1fad24db5d2f47075b08da5e
Sami Munir <sami.munir2001@gmail.com>
2024-10-12 02:24:24 -0400

    implementing function AO_planner.nearest_node() to find the nearest node in the tree given a sampled_configuration

--- Rutgers/CS460/assignment_2/AO_planner.py
+++ Rutgers/CS460/assignment_2/AO_planner.py
@@ -13,3 +13,10 @@ def is_collision_free(config, environment):
         if (abs(x - config[0]) * 2 < width) and (abs(y - config[1]) * 2 < height):
             return False
     return True
+
+# Function to find the nearest node in the tree
+def nearest_node(tree, sampled_config):
+    nodes = NP.array([node['config'] for node in tree])
+    distances = NP.linalg.norm(nodes - NP.array(sampled_config), axis=1)
+    nearest_idx = NP.argmin(distances)
+    return nearest_idx
