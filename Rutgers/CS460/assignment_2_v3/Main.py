798e142341bc8911e0311c50a59f3d472efef7f1
Sami Munir <sami.munir2001@gmail.com>
2024-10-24 03:56:41 -0400

    component_1: visualize_scene() complete + component_1 100% done

--- Rutgers/CS460/assignment_2_v3/Main.py
+++ Rutgers/CS460/assignment_2_v3/Main.py
@@ -64,12 +64,12 @@ def test_component_i(number_of_environments: int, initial_number_of_obstacles: i
         
         scene_to_file(environment = ENVIRONMENT, filename = f'environment_{(i + 1)}_{number_of_obstacles}.txt')
         
-    #     visualize_scene(environment = ENVIRONMENT)
+        visualize_scene(environment = ENVIRONMENT)
         
         number_of_obstacles += number_of_obstacles_step_size
     
-    # LOADED_ENVIRONMENT = scene_from_file(filename = 'environment_1_10.txt')
-    # visualize_scene(environment = LOADED_ENVIRONMENT)
+    LOADED_ENVIRONMENT = scene_from_file(filename = 'environment_1_10.txt')
+    visualize_scene(environment = LOADED_ENVIRONMENT)
 
 """
     function main():
--- Rutgers/CS460/assignment_2_v3/component_1.py
+++ Rutgers/CS460/assignment_2_v3/component_1.py
@@ -31,7 +31,7 @@ OBSTACLE_MAX_SIZE = 2.0
     - this function will generate a randomized environment with the number of randomized obstacles = number_of_obstacles.
     - this function returns a list of random obstacles.
     - each obstacle is a tuple in the form:
-        > (x, y, width, height, theta, corners)
+        > (x, y, width, height, theta, c1x, c1y, c2x, c2y, c3x, c3y, c4x, c4y)
             * corners are the 4 corners of the obstacle rectangle.
             * will use utils.get_polygon_corners() to compute corners() tuple.
 """
@@ -49,8 +49,12 @@ def generate_environment(number_of_obstacles: int) -> list:
         
         corners = get_polygon_corners(center = (x, y), width = width, height = height, theta = theta)
         c1, c2, c3, c4 = corners
+        c1x, c1y = c1
+        c2x, c2y = c2
+        c3x, c3y = c3
+        c4x, c4y = c4
         
-        OBSTACLE = (x, y, width, height, theta, c1, c2, c3, c4)
+        OBSTACLE = (x, y, width, height, theta, c1x, c1y, c2x, c2y, c3x, c3y, c4x, c4y)
         
         ENVIRONMENT.append(OBSTACLE)
         
@@ -69,7 +73,7 @@ def scene_to_file(environment: list, filename: str) -> None:
     
     with open(filename, 'w') as FILE:
         for OBSTACLE in environment:
-            FILE.write(f'{OBSTACLE[0]}, {OBSTACLE[1]}, {OBSTACLE[2]}, {OBSTACLE[3]}, {OBSTACLE[4]}, {OBSTACLE[5]}, {OBSTACLE[6]}, {OBSTACLE[7]}, {OBSTACLE[8]}\n')
+            FILE.write(f'{OBSTACLE[0]}, {OBSTACLE[1]}, {OBSTACLE[2]}, {OBSTACLE[3]}, {OBSTACLE[4]}, {OBSTACLE[5]}, {OBSTACLE[6]}, {OBSTACLE[7]}, {OBSTACLE[8]}, {OBSTACLE[9]}, {OBSTACLE[10]}, {OBSTACLE[11]}, {OBSTACLE[12]}\n')
 
     print(f'\tenvironment saved to FILE <{filename}>.')
     
@@ -95,22 +99,28 @@ def scene_from_file(filename: str) -> list:
             width = float(VALUES[2])
             height = float(VALUES[3])
             theta = float(VALUES[4])
-            c1 = float(VALUES[5])
-            c2 = float(VALUES[6])
-            c3 = float(VALUES[7])
-            c4 = float(VALUES[8])
+            c1x = float(VALUES[5])
+            c1y = float(VALUES[6])
+            c2x = float(VALUES[7])
+            c2y = float(VALUES[8])
+            c3x = float(VALUES[9])
+            c3y = float(VALUES[10])
+            c4x = float(VALUES[11])
+            c4y = float(VALUES[12])
             
-            OBSTACLE = (x, y, width, height, theta, c1, c2, c3, c4)
+            
+            OBSTACLE = (x, y, width, height, theta, c1x, c1y, c2x, c2y, c3x, c3y, c4x, c4y)
             
             OBSTACLES.append(OBSTACLE)
     
-    TIME.sleep(2)
     print(f'\tenvironment loaded from FILE <{filename}>.')
     
     return OBSTACLES
 
 """
     function visualize_scene(environment: list):
+    - this function will visualize the environment as described in the environment list (of obstacles).
+    - this function only uses the obstacle's (x, y, width, height, theta) values.
 """
 def visualize_scene(environment: list) -> None:
     print(f'\nvisualize_scene() called...')
@@ -118,9 +128,9 @@ def visualize_scene(environment: list) -> None:
     FIGURE, AXES = PLT.subplots()
     
     for OBSTACLE in environment:
-        x, y, width, height, theta = OBSTACLE
+        x, y, width, height, theta, _, _, _, _, _, _, _, _ = OBSTACLE
         
-        OBSTACLE_RECTAGNGLE = PTCHS.Rectangle((x, y), width, height, angle = NP.rad2deg(theta), color = '#ff0000', edgecolor = '#ff0000')
+        OBSTACLE_RECTAGNGLE = PTCHS.Rectangle((x, y), width, height, angle = NP.rad2deg(theta), color = '#ff0000', edgecolor = '#ff0000', alpha = 0.75)
         
         AXES.add_patch(OBSTACLE_RECTAGNGLE)
     
@@ -131,3 +141,5 @@ def visualize_scene(environment: list) -> None:
     PLT.title(f'Randomly generated environment: {len(environment)} obstacles')
     
     PLT.show(block = False)
+    PLT.pause(2)
+    PLT.close(FIGURE)
